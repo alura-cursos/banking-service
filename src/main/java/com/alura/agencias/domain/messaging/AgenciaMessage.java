8b40887936365dfdb89e6c9bc7a310e4f155ac6b
Joao Victor <martins.joao@ifood.com.br>
2025-06-27 17:56:26 -0300

    adding avro

--- src/main/java/com/alura/agencias/domain/messaging/AgenciaMessage.java
+++ src/main/java/com/alura/agencias/domain/messaging/AgenciaMessage.java
@@ -6,23 +6,17 @@ public class AgenciaMessage {
 
     }
 
-    public AgenciaMessage(Long id, String nome, String razaoSocial, String cnpj, String situacaoCadastral) {
-        this.id = id;
+    public AgenciaMessage(String nome, String razaoSocial, String cnpj, String situacaoCadastral) {
         this.nome = nome;
         this.razaoSocial = razaoSocial;
         this.cnpj = cnpj;
         this.situacaoCadastral = situacaoCadastral;
     }
 
-    private Long id;
     private String nome;
     private String razaoSocial;
     private String cnpj;
     private String situacaoCadastral;
-
-    public Long getId() {
-        return id;
-    }
 
     public String getNome() {
         return nome;
--- src/main/java/com/alura/agencias/service/messaging/RemoverAgenciaService.java
+++ src/main/java/com/alura/agencias/service/messaging/RemoverAgenciaService.java
@@ -1,10 +1,12 @@
 package com.alura.agencias.service.messaging;
 
+import br.com.alura.Agencia;
 import com.alura.agencias.domain.messaging.AgenciaMessage;
 import com.alura.agencias.repository.AgenciaRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -22,13 +24,20 @@ public class RemoverAgenciaService {
 
     @WithTransaction
     @Incoming("remover-agencia-channel")
-    public Uni<Void> consumirMensagem(String mensagem) {
+    public Uni<Void> consumirMensagem(Agencia mensagem) {
         try {
-            AgenciaMessage agenciaMessage = objectMapper.readValue(mensagem, AgenciaMessage.class);
+            Log.info(mensagem);
+            //AgenciaMessage agenciaMessage = objectMapper.readValue(mensagem, AgenciaMessage.class);
+            AgenciaMessage agenciaMessage =
+                    new AgenciaMessage(mensagem.getNome(),
+                            mensagem.getRazaoSocial(),
+                            mensagem.getCnpj(),
+                            mensagem.getSituacaoCadastral());
             return agenciaRepository.findByCnpj(agenciaMessage.getCnpj())
                     .onItem().ifNotNull().transformToUni(agencia ->
                         agenciaRepository.deleteById(agencia.getId())).replaceWithVoid();
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
+            Log.error(e.getMessage());
             return Uni.createFrom().failure(e);
         }
     }
