539dd207dd2d8935c31bed7337c27c3c1a84692b
Joao Victor <martins.joao@ifood.com.br>
2025-09-09 12:10:05 -0300

    quinta aula curso 3

--- src/main/java/com/alura/agencias/service/AgenciaService.java
+++ src/main/java/com/alura/agencias/service/AgenciaService.java
@@ -5,9 +5,14 @@ import com.alura.agencias.domain.http.AgenciaHttp;
 import com.alura.agencias.domain.http.SituacaoCadastral;
 import com.alura.agencias.exception.AgenciaNaoAtivaOuNaoEncontradaException;
 import com.alura.agencias.repository.AgenciaRepository;
+import com.alura.agencias.service.cache.RedisCacheService;
 import com.alura.agencias.service.http.SituacaoCadastralHttpService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.logging.Log;
@@ -20,10 +25,16 @@ public class AgenciaService {
 
     private final AgenciaRepository agenciaRepository;
     private final MeterRegistry meterRegistry;
+    private final RedisCacheService redisCacheService;
+    private final ObjectMapper objectMapper;
+    private final Tracer tracer;
 
-    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry) {
+    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry, RedisCacheService redisCacheService, Tracer tracer) {
         this.agenciaRepository = agenciaRepository;
         this.meterRegistry = meterRegistry;
+        this.redisCacheService = redisCacheService;
+        this.objectMapper = new ObjectMapper();
+        this.tracer = tracer;
     }
 
     @RestClient
@@ -31,12 +42,22 @@ public class AgenciaService {
 
     @WithTransaction // to do -> usando o hibernate sem panache ainda precisaria manter a transação aberta com o @WithTransaction
     public Uni<Void> cadastrar(Agencia agencia) {
+        Span span = tracer.spanBuilder("cadastrarAgencia").startSpan();
+        span.setAttribute("agencia.cnpj", agencia.getCnpj());
         Counter counter = this.meterRegistry.counter("agencia_nao_adicionada_count");
         return situacaoCadastralHttpService.buscarPorCnpj(agencia.getCnpj())
                 .onItem()
                     .ifNull().failWith(new AgenciaNaoAtivaOuNaoEncontradaException())
                     .invoke(a -> Log.info("Agencia com CNPJ " + a.getCnpj() + " foi encontrada"))
                     .invoke(t -> counter.increment())
+                    .invoke(a -> {
+                        try(Scope scope = span.makeCurrent()) {
+                            span.addEvent("Agencia encontrada");
+                            span.setAttribute("agencia.encontrada", a.getCnpj());
+                        }
+                    }).eventually(() -> {
+                        span.end();
+                })
                 .onItem().transformToUni(agenciaHttpTransformada -> persistirSeEstaAtiva(agenciaHttpTransformada, agencia, counter));
     }
 
@@ -47,7 +68,7 @@ public class AgenciaService {
                     .invoke(a -> Log.info("Agencia com CNPJ " + agencia.getCnpj() + " foi adicionada"))
                     .replaceWithVoid();
         } else {
-            Log.info("Agencia com CNPJ " + agencia.getCnpj() + " não ativa"); // to do -> pesquisar aqui tb.
+            Log.error("Agencia com CNPJ " + agencia.getCnpj() + " não ativa"); // to do -> pesquisar aqui tb.
             counter.increment();
             return Uni.createFrom().failure(new AgenciaNaoAtivaOuNaoEncontradaException());
         }
@@ -55,13 +76,44 @@ public class AgenciaService {
 
     @WithSession
     public Uni<Agencia> buscarPorId(Long id) {
-        return agenciaRepository.findById(id);
+        String key = "agencia_" + id;
+        return buscarNoCache(key).onItem().ifNull().switchTo(buscarNoBanco(key, id));
     }
+
+    public Uni<Agencia> buscarNoCache(String key) {
+        this.meterRegistry.counter("agencia_busca_cache_count").increment();
+        return redisCacheService.get(key)
+                .onItem().ifNotNull().transform(agencia -> {
+                    try {
+                        Log.info("Agência encontrada no cache");
+                        return objectMapper.readValue(agencia, Agencia.class);
+                    } catch (Exception e) {
+                        Log.error("Objeto não pode ser encontrado");
+                        return null;
+                    }
+                });
+    }
+
+    public Uni<Agencia> buscarNoBanco(String key, Long id) {
+        return agenciaRepository.findById(id)
+                .onItem().ifNotNull().call(agencia -> {
+                    try {
+                        Log.info("Setando informação no cache");
+                        return redisCacheService.set(key, objectMapper.writeValueAsString(agencia), 3600);
+                    } catch (Exception e) {
+                        return Uni.createFrom().failure(e);
+                    }
+                });
+    }
+
 
     @WithTransaction
     public Uni<Void> deletar(Long id) {
+        String key = "agencia_" + id;
+
         return agenciaRepository.deleteById(id)
-                .invoke(a -> Log.info("A agência foi deletada")) // to do -> verificar como poderia usar log level
+                .call(a -> redisCacheService.del(key))
+                .invoke(a -> Log.info("A agência foi deletada"))
                 .replaceWithVoid();
     }
 
